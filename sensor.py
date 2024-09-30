1f04f42cd02974b057578d91666563d8f4aa0463
Michal Hynek <k3dt@185-244-191-12.amigonet.cz>
2024-09-30 15:17:07 +0200

    fix

--- sensor.py
+++ sensor.py
@@ -8,9 +8,11 @@ from homeassistant.helpers.update_coordinator import (
 )
 from homeassistant.helpers import aiohttp_client
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN, URL
+from .const import URL
+from zoneinfo import ZoneInfo
 
 _LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     session = aiohttp_client.async_get_clientsession(hass)
@@ -18,14 +20,17 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     await coordinator.async_config_entry_first_refresh()
 
     sensors = [
-        AmigonetOTESensor(coordinator, 'current_ote_price', 'Aktuální OTE cena', 'CZK'),
-        AmigonetOTESensor(coordinator, 'average_ote_price', 'Průměrná OTE cena', 'CZK'),
+        AmigonetOTESensor(coordinator, "current_ote_price", "Aktuální OTE cena", "CZK"),
+        AmigonetOTESensor(coordinator, "average_ote_price", "Průměrná OTE cena", "CZK"),
     ]
 
     for i in range(1, 7):
-        sensors.append(AmigonetOTEHourSensor(coordinator, f'min{i}', f'Min OTE {i} hodin'))
+        sensors.append(
+            AmigonetOTEHourSensor(coordinator, f"min{i}", f"Min OTE ({i} hod)")
+        )
 
     async_add_entities(sensors)
+
 
 class AmigonetOTESensor(CoordinatorEntity, SensorEntity):
     def __init__(self, coordinator, key, name, unit):
@@ -40,7 +45,7 @@ class AmigonetOTESensor(CoordinatorEntity, SensorEntity):
 
     @property
     def unique_id(self):
-        return f'amigonet_ote_{self._key}'
+        return self._key
 
     @property
     def state(self):
@@ -50,29 +55,40 @@ class AmigonetOTESensor(CoordinatorEntity, SensorEntity):
     def unit_of_measurement(self):
         return self._unit
 
+
 class AmigonetOTEHourSensor(CoordinatorEntity, SensorEntity):
     def __init__(self, coordinator, key, name):
         super().__init__(coordinator)
         self._key = key
         self._name = name
+        self._attributes = {"hodiny": []}
 
     @property
     def name(self):
         return self._name
 
     @property
+    def extra_state_attributes(self):
+        return self._attributes
+
+    @property
     def unique_id(self):
-        return f'min_ote_{self._key}_hours'
+        return "min_ote_" + self._key + "_hours"
 
     @property
     def state(self):
-        current_hour = datetime.now().hour
+        prague_tz = ZoneInfo("Europe/Prague")
+        current_time = datetime.now(prague_tz)
+        current_hour = current_time.hour
         hours_list = self.coordinator.data.get(self._key, [])
+        self._attributes["hodiny"] = hours_list
+        # _LOGGER.debug(f"{self._name} - Hours: {hours_list}, Current: {current_hour}")
         return current_hour in hours_list
 
     @property
     def icon(self):
         return "mdi:clock"
+
 
 class AmigonetOTEDataUpdateCoordinator(DataUpdateCoordinator):
     def __init__(self, hass: HomeAssistant, session):
@@ -91,6 +107,7 @@ class AmigonetOTEDataUpdateCoordinator(DataUpdateCoordinator):
                 if response.status != 200:
                     raise UpdateFailed(f"Error fetching data: {response.status}")
                 self.data = await response.json()
+                # _LOGGER.debug(f"Data: {self.data}")
             return self.data
         except Exception as err:
             raise UpdateFailed(f"Error fetching data: {err}")
